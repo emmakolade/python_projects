ad3c19fabf9c63e1de57886c31c08df8d56aea51
emmakolade <emmakolade@gmail.com>
2022-09-12 09:15:20 +0100

    guess

--- python_beginner_project/guess_the_number_computer.py
+++ python_beginner_project/guess_the_number_computer.py
@@ -16,3 +16,24 @@ def guess(x):
     print(f" greattttt. you got the number {random_n}")
 
 guess(10)
+
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high: # there will be an error from .randint if the low and high is the same number
+            n_guess = random.randint(low, high)
+        else:
+            n_guess = low # can also be high
+        feedback = input(f"is {guess} too high (H), too low (L), or correct (C)?").lower()
+        if feedback == "h":
+            high = n_guess - 1
+        elif feedback == "l":
+            low = n_guess + 1
+
+    print(f" the computer guessed your number, {n_guess}, correctly")
+
+computer_guess(10)
